9ec2f4026d06058022e76d68fe9703985c694c31
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-04-20 17:27:06 +0200

    añadida la función que calcula la media

--- analisis.py
+++ analisis.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# esta funcion leera el archivo y lo guardara en una variable
+# esta función leerá el archivo y lo guardará en una variable
 def leer_archivo(archivo):
     datos = pd.read_csv(archivo, sep=',', header=None, skiprows=1)
     return datos
@@ -14,3 +14,13 @@ def diagrama_barras(datos, titulo, x, y):
     plt.xlabel("Opinión"), plt.ylabel("Cantidad de votantes")
     plt.show()
 diagrama_barras(l, 'Estudio sobre las críticas de una película', 0, 1)
+# función para calcular la media
+def media(datos):
+    productos = []
+    for i in range(len(datos[0])):
+        productos.append(datos[0][i]*datos[1][i])
+    df = pd.DataFrame(datos) 
+    df.insert(2,"Productos",productos,True)
+    print(np.mean(df["Productos"])) 
+
+print("La media de las críticas es: "), media(l)
