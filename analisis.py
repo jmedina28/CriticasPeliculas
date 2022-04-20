cb8ac737965a2f82a79551302a5ebf25f06d899d
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-04-20 13:32:36 +0200

    convierto los datos del csv en listas

--- analisis.py
+++ analisis.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 # esta función se encargará de leer los datos del archivo .csv
 def lectura(archivo):
-    data = pd.read_csv(archivo, sep=',', header=None)
+    data = pd.read_csv(archivo, sep=',', header=None, skiprows= [0])
     return data
 print(lectura('criticas.csv'))
+# esta función se encargará de convertir los datos de la columna 0 a una lista
+def convertir(data):
+    lista0 = data[0].tolist()
+    lista1 = data[1].tolist()
+    return lista0, lista1
+print(convertir(lectura('criticas.csv')))
