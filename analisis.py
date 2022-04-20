9baf5f8779d888418f92ff4ecc1046ace4763726
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-04-20 17:01:28 +0200

    rehago la función

--- analisis.py
+++ analisis.py
@@ -1,14 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-# esta función se encargará de leer los datos del archivo .csv
-def lectura(archivo):
-    data = pd.read_csv(archivo, sep=',', header=None, skiprows= [0])
-    return data
-print(lectura('criticas.csv'))
-# esta función se encargará de convertir los datos de la columna 0 a una lista
-def convertir(data):
-    lista0 = data[0].tolist()
-    lista1 = data[1].tolist()
-    return lista0, lista1
-print(convertir(lectura('criticas.csv')))
+
+# esta funcion leera el archivo y lo guardara en una variable
+def leer_archivo(archivo):
+    datos = pd.read_csv(archivo, sep=',', header=None, skiprows=1)
+    return datos
+l = leer_archivo('criticas.csv')
+print(l)
